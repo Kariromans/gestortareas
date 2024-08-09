3d3b5a13a3cfeb805b09ca912072164f8de6a7c6
Kari Romans <karinaromansalgado@gmail.com>
2024-08-09 14:21:40 -0600

    termino de ui y task, inicio de index

--- src/task.js
+++ src/task.js
@@ -1,2 +1,33 @@
-sk = JSON.parse(localStorage.getIem("tasks")) || [];
+let tasks = JSON.parse(localStorage.getIem("tasks")) || [];
+// traernos la lista de tareas de almacenamiento local//
 
+//Funcion para agregar una tarea//
+export const addTask = (task) => {
+    const newtask = {
+        id: Date.now(),
+        text: task,
+        completed: false,
+    };
+    tasks.push(newtask);
+    localStorage.setItem("tasks", JSON.stringify(task));
+};
+
+//funcion para eliminar una tarea//
+export const deleteTask = (id) => {
+    tasks = tasks.filter((task) => task.id !== parseInt(id));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+//funcion para actualizar la tarea//
+export const toggleTask = (id) => {
+    tasks = tasks.map((task) => {
+        if (task.id === parseInt(id)) {
+            task.completed = !task.completed;
+        }
+        return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// funcion para llevar las tareas //
+export const getTasks =() => tasks; 
---
+++ src/ui.js
@@ -0,0 +1,23 @@
+import { getTasks } from "./task";
+
+//funcion para mostrarle al usuario las tareas y lo que hacemos con ellas//
+export const renderTasks = () => {
+    const TaskList = document.getElementById("task-list");
+    TaskList.innerHTML = "";
+    const tasks = getTasks();
+    tasks.forEach((task) => {
+        const li = document.createElement("Li");
+        li.setAttribute("data-id", task.id)
+
+        // añadir clase solo si la tarea esta completada //
+        if (task.complete === true) {
+            li.classList.add("completed");
+        }
+        li.innerHTML = `
+        ${task.text}
+        <button class= "delete"> Eliminar </button> 
+        <button class ="toggle"> ${task.completed === true? "Deshacer" : "completar" }  </button>
+        `;
+        TaskList.appendChild(Li);
+    });
+};
